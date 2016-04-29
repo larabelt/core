eae8d2a2c1789a59066eab7e09afbc81c32815c1
Matt Crowe <matthew.p.crowe@gmail.com>
2016-04-29 14:36:56 -0400

    init

---
+++ src/Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ohio\Base;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Hello
+{
+
+    public static function run()
+    {
+        return 'world 222';
+    }
+
+}
