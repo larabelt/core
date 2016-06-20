8e2a4c2fa3d069f2a0ad04f859890a2deb8bea74
Matt Crowe <matthew.p.crowe@gmail.com>
2016-06-20 17:21:14 -0400

    base controller

---
+++ src/Http/Controllers/BaseController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ohio\Base\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//use App\Http\Requests;
+//use App\Http\Requests\PostCreateRequest;
+//use App\Http\Requests\PostUpdateRequest;
+
+use Ohio\Base\Domain\BaseRepository;
+use Ohio\Base\Domain\BaseValidator;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+
+class BaseController extends Controller
+{
+
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var BaseRepository
+     */
+    protected $repository;
+
+    /**
+     * @var BaseValidator
+     */
+    protected $validator;
+
+    /**
+     * @var mixed
+     */
+    protected $data;
+
+}
