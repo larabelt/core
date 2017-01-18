a1a6a7ff8341652003c274b089d18b6944d24b6a
Matt Crowe <matthew.p.crowe@gmail.com>
2017-01-17 21:22:47 -0500

    unit tests

--- tests/base/Http/Controllers/Behaviors/PositionableControllerTraitTest.php
+++ tests/base/Http/Controllers/Behaviors/PositionableControllerTraitTest.php
@@ -5,7 +5,6 @@ use Ohio\Core\Base\Testing\CommonMocks;
 use Ohio\Core\Base\Http\Controllers\Behaviors\PositionableControllerTrait;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Rutorika\Sortable\MorphToSortedMany;
 
@@ -28,25 +27,32 @@ class PositionableControllerTraitTest extends \PHPUnit_Framework_TestCase
         $entityToMove = new PositionableChildModelStub(['id' => 3]);
         $entityInDesiredPosition = new PositionableChildModelStub(['id' => 1]);
 
-        $collection = new Collection([
-            new PositionableChildModelStub(['id' => 1]),
-            new PositionableChildModelStub(['id' => 3]),
-        ]);
+        # moveAfter
+        $collection = m::mock(Collection::class);
+        $collection->shouldReceive('where')->once()->with('id', 3)->andReturnSelf();
+        $collection->shouldReceive('first')->once()->andReturn($entityToMove);
+        $collection->shouldReceive('where')->once()->with('id', 1)->andReturnSelf();
+        $collection->shouldReceive('first')->once()->andReturn($entityInDesiredPosition);
 
         $relation = m::mock(MorphToSortedMany::class);
+        $relation->shouldReceive('moveAfter')->once()->with($entityToMove, $entityInDesiredPosition)->andReturnNull();
 
-        //$relation->shouldReceive('moveAfter')->with($entityToMove, $entityInDesiredPosition)->andReturnNull();
-        //$relation->shouldReceive('moveBefore')->with($entityToMove, $entityInDesiredPosition)->andReturnNull();
-
-        $relation->shouldReceive('moveAfter')->once()->withAnyArgs()->andReturnNull();
-        $relation->shouldReceive('moveBefore')->once()->withAnyArgs()->andReturnNull();
-
-        # moveAfter
         $request = new Request(['move' => 'after', 'position_entity_id' => 1]);
+
         $controller->repositionEntity($request, 3, $collection, $relation);
 
         # moveBefore
+        $collection = m::mock(Collection::class);
+        $collection->shouldReceive('where')->once()->with('id', 3)->andReturnSelf();
+        $collection->shouldReceive('first')->once()->andReturn($entityToMove);
+        $collection->shouldReceive('where')->once()->with('id', 1)->andReturnSelf();
+        $collection->shouldReceive('first')->once()->andReturn($entityInDesiredPosition);
+
+        $relation = m::mock(MorphToSortedMany::class);
+        $relation->shouldReceive('moveBefore')->once()->with($entityToMove, $entityInDesiredPosition)->andReturnNull();
+
         $request = new Request(['move' => 'before', 'position_entity_id' => 3]);
+
         $controller->repositionEntity($request, 1, $collection, $relation);
     }
 
@@ -55,11 +61,9 @@ class PositionableControllerTraitTest extends \PHPUnit_Framework_TestCase
 class PositionableChildModelStub
 {
     public $id;
-    public $attributes;
 
     public function __construct($attributes)
     {
-        $this->attributes = $attributes;
         $this->id = array_get($attributes, 'id');
     }
 }
