ee408e423160f6d84ee5033424441e0d186b5c2d
Matt Crowe <matthew.p.crowe@gmail.com>
2017-01-19 17:25:42 -0500

    unit tests

--- tests/base/Http/Requests/FormRequestTest.php
+++ tests/base/Http/Requests/FormRequestTest.php
@@ -1,13 +1,15 @@
 <?php
 
 use Ohio\Core\Base\Http\Requests\FormRequest;
+use Illuminate\Validation\Rules;
 
 class FormRequestTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::rules()
-     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::wantsJson()
-     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::authorize()
+     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::rules
+     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::wantsJson
+     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::authorize
+     * @covers \Ohio\Core\Base\Http\Requests\FormRequest::ruleExists
      */
     public function test()
     {
@@ -16,6 +18,7 @@ class FormRequestTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([], $request->rules());
         $this->assertTrue($request->wantsJson());
         $this->assertTrue($request->authorize());
+        $this->assertInstanceOf(Rules\Exists::class, $request->ruleExists('table', 'column', ['foo' => 'bar']));
     }
 
 }
