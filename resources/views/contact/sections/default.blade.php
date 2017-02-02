c9f5d04e91789b08e5d951cb44cec6876a028ec1
Matt Crowe <github.mpc@zym.me>
2017-02-02 22:27:55 +0000

    page sections init

---
+++ resources/views/contact/sections/default.blade.php
@@ -0,0 +1,6 @@
+<div class="section section-embed section-embed-contact {{ $section->param('class') }}">
+    @include('ohio-content::section.sections._header')
+    @include('ohio-content::section.sections._body')
+    @include('ohio-core::contact.web._show')
+    @include('ohio-content::section.sections._footer')
+</div>
