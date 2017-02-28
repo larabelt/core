e7dbbf45a46f0b0909a5a32dcf9e5c4b62744da7
Matt Crowe <github.mpc@zym.me>
2017-02-28 19:40:27 +0000

    page sections UI

--- resources/views/contact/sections/default.blade.php
+++ resources/views/contact/sections/default.blade.php
@@ -1,6 +1,6 @@
 <div class="section section-embed section-embed-contact {{ $section->param('class') }}">
-    @include('belt-content::sections.sections._header')
-    @include('belt-content::sections.sections._body')
+    @include('belt-content::sections.sections._heading')
+    @include('belt-content::sections.sections._before')
     @include('belt-core::contact.web.show')
-    @include('belt-content::sections.sections._footer')
+    @include('belt-content::sections.sections._after')
 </div>
