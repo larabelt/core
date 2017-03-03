2bd335ce6c9cf2350ee28f6508862258c58f5ba9
rob.lasota <rob.lasota@fahlgren.com>
2017-03-02 23:30:29 -0500

    updated tinymce to update the vue model

--- resources/js/directives/tinymce.js
+++ resources/js/directives/tinymce.js
@@ -1,16 +1,16 @@
 import slugify from 'slugify';
 
 export default {
+    inserted: function (el, binding, vnode, oldVnode) {
+        let expression = vnode.data.directives.find(function(o) {
+            return o.name === 'model';
+        }).expression
 
-    inserted: function (el, binding) {
-
-        let slugged = typeof binding.value == 'string' ? slugify(binding.value) : '';
-        let unique = 'editor-' + slugged;
-
-        $(el).addClass(unique);
+        //@todo[lasota] Need to refactor this to not depend on 2 steps.
+        expression = expression.split('.');
 
         tinymce.init({
-            selector: '.' + unique,
+            target: el,
             height: 300,
             menubar: false,
             plugins: [
@@ -18,7 +18,15 @@ export default {
                 'searchreplace visualblocks code fullscreen',
                 'insertdatetime media table contextmenu paste code'
             ],
-            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code',
+            init_instance_callback: function (editor) {
+                editor.on('keyup', function (e) {
+                    vnode.context[expression[0]][expression[1]] = editor.getContent();
+                });
+                editor.on('NodeChange', function (e) {
+                    vnode.context[expression[0]][expression[1]] = editor.getContent();
+                });
+            }
         });
     }
 }
