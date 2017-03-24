4c27a3fde8b18d88f663975de5ae5f389d0e5e3e
Matt Crowe <github.mpc@zym.me>
2017-03-24 19:45:59 +0000

    expression to arguments helper

--- src/Helpers/StrHelper.php
+++ src/Helpers/StrHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Belt\Core\Helpers;
 
 /**
@@ -17,6 +18,72 @@ class StrHelper
         json_decode($s);
 
         return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    /**
+     * @param $expression
+     * @param $count
+     * @return array
+     */
+    public static function strToArguments($expression, $count = 1)
+    {
+        $arguments = [];
+        foreach (explode(',', $expression) as $str) {
+            $str = trim($str, "' ");
+            $argument = static::strToArray($str) ?: $str;
+            $arguments[] = $argument;
+        }
+
+        while (count($arguments) < $count) {
+            $arguments[] = null;
+        }
+
+        return $arguments;
+    }
+
+    /**
+     * @source http://stackoverflow.com/questions/11267434/php-how-to-turn-a-string-that-contains-an-array-expression-in-an-actual-array/18187993#18187993
+     * @param $str
+     * @return mixed
+     */
+    public static function strToArray($str)
+    {
+        if (!function_exists("token_get_all")) {
+            return false;
+        }
+
+        $code = "return $str;";
+
+        $tokens = token_get_all("<?php $code ?>");
+        foreach ($tokens as $token) {
+
+            $type = $token[0];
+
+            if (is_long($type)) {
+                if (in_array($type, [
+                    T_OPEN_TAG,
+                    T_RETURN,
+                    T_WHITESPACE,
+                    T_ARRAY,
+                    T_LNUMBER,
+                    T_DNUMBER,
+                    T_CONSTANT_ENCAPSED_STRING,
+                    T_DOUBLE_ARROW,
+                    T_CLOSE_TAG,
+                    T_NEW,
+                    T_DOUBLE_COLON,
+                ])) {
+                    continue;
+                }
+
+                return false;
+            }
+
+            $array = eval($code);
+
+            return is_array($array) ? $array : false;
+        }
+
     }
 
 }
