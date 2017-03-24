1949e4efaf88387360186ce283fd50a4659ef106
Matt Crowe <github.mpc@zym.me>
2017-03-24 01:52:23 +0000

    rm archived folders

--- resources/js/archive/components/user/role/templates/index.js
+++
@@ -1,8 +0,0 @@
-export default `
-    <div class="form-roles">
-        <div v-for="role in items">
-            <button v-if="isAttached(role)" class="active btn btn-sm" v-on:click="detach(role.id)">{{ role.name }}</button>
-            <button v-else class="btn btn-sm" v-on:click="attach(role.id)">{{ role.name }}</button>
-        </div>
-    </div>
-`;
