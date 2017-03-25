c43b916f7ba7c622d55ef9d51f21efbf1186e4b7
Matt Crowe <github.mpc@zym.me>
2017-03-25 14:54:35 +0000

    move morphsort to separate trait

---
+++ src/Behaviors/HasSortableTrait.php
@@ -0,0 +1,23 @@
+<?php
+namespace Belt\Core\Behaviors;
+
+use Rutorika\Sortable\MorphToSortedManyTrait;
+
+trait HasSortableTrait
+{
+    use MorphToSortedManyTrait;
+
+    /**
+     * @todo deprecate
+     *
+     * Eloquent renamed getBelongsToManyCaller to guessBelongsToManyRelation
+     * and the package Rutorika\Sortable currently expects the old name to exist
+     *
+     * @return mixed
+     */
+    protected function getBelongsToManyCaller()
+    {
+        return $this->guessBelongsToManyRelation();
+    }
+
+}
