d5423d94bbe2402ab5ef694ffb37cdd7d8e24846
Matt Crowe <github.mpc@zym.me>
2017-04-09 13:34:25 +0000

    api search and better *.searchable

--- src/Behaviors/TypeTrait.php
+++ src/Behaviors/TypeTrait.php
@@ -1,10 +1,20 @@
 <?php
+
 namespace Belt\Core\Behaviors;
 
+/**
+ * Class TypeTrait
+ * @package Belt\Core\Behaviors
+ */
 trait TypeTrait
 {
     public function getTypeAttribute()
     {
         return $this->getTable();
+    }
+
+    public function getMorphClassAttribute()
+    {
+        return $this->getMorphClass();
     }
 }
