90bc4ad347297677de97f8dfcda5f2679d67f07d
rob.lasota <rob.lasota@fahlgren.com>
2017-06-19 16:53:44 -0400

    updated tinymce to extend valid elements instead of overwriting them

--- resources/js/directives/tinymce.js
+++ resources/js/directives/tinymce.js
@@ -25,7 +25,7 @@ export default {
                     _.set(vnode.context, expression, editor.getContent());
                 });
             },
-            valid_elements: 'a[role|aria*|tabindex]'
+            extended_valid_elements: 'a[role|aria*|tabindex]'
         }).then(response => {
             el.dataset.tinymceid = response[0].id;
             el.dataset.tinymcerecheck = el.value != '';
