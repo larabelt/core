90840730a6381638d165b78fd424debe3694aa51
Matt Crowe <github.mpc@zym.me>
2017-09-08 21:01:50 +0000

    version update

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.2.6';
+    const VERSION = '1.2.7';
 
     /**
      * The policy mappings for the application.
