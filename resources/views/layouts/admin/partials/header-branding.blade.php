2eeccd231e2cb6aeb40bc6bc762eb130f83ba56c
Matt Crowe <github.mpc@zym.me>
2017-09-08 16:21:05 +0000

    tweak header-branding link

--- resources/views/layouts/admin/partials/header-branding.blade.php
+++ resources/views/layouts/admin/partials/header-branding.blade.php
@@ -1,5 +1,5 @@
 <!-- Logo -->
-<a href="/admin" class="logo">
+<a href="/" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>LB</b></span>
     <!-- logo for regular state and mobile devices -->
