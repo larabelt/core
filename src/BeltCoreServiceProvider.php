06df3e8d5e805b67a2524b7bdb160fe4ac308975
Matt Crowe <github.mpc@zym.me>
2017-09-12 19:20:50 +0000

    events api data

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.2.8';
+    const VERSION = '1.2.9';
 
     /**
      * The policy mappings for the application.
