e17916028d7cd2fcc9a7f0a519feaac776e38f75
Matt Crowe <github.mpc@zym.me>
2017-10-17 14:39:51 +0000

    spot isSearchableArray() bug

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.4';
+    const VERSION = '1.4.5';
 
     /**
      * The policy mappings for the application.
