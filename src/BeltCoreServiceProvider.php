47571b99f408ba7a0b7608575865a371bd386bf8
Matt Crowe <github.mpc@zym.me>
2017-10-17 15:20:49 +0000

    remove debug code

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.5';
+    const VERSION = '1.4.6';
 
     /**
      * The policy mappings for the application.
