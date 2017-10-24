96b136403c33f851055df2de54f4579063c42f92
Matt Crowe <github.mpc@zym.me>
2017-10-24 16:32:26 +0000

    search paginator  arg. handles UX

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.13';
+    const VERSION = '1.4.14';
 
     /**
      * The policy mappings for the application.
