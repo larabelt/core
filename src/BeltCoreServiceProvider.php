4457d91e59a0bd4df5e1a4f75e5ac9aebfeec3a1
Matt Crowe <github.mpc@zym.me>
2017-10-31 18:21:08 +0000

    remove spot debug code

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.16';
+    const VERSION = '1.4.17';
 
     /**
      * The policy mappings for the application.
