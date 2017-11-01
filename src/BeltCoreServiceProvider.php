9e4856e75d0744bad8e64260dadfbf7e4517a763
Matt Crowe <github.mpc@zym.me>
2017-11-01 19:48:55 +0000

    glue modifier account for url encoding

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.19';
+    const VERSION = '1.4.20';
 
     /**
      * The policy mappings for the application.
