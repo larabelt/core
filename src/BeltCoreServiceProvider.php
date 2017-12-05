c08809c416b6656397fdfaa480dc6859cd51bd1f
Matt Crowe <github.mpc@zym.me>
2017-12-05 20:38:37 +0000

    admin filters

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -21,7 +21,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.41';
+    const VERSION = '1.4.42';
 
     /**
      * The policy mappings for the application.
