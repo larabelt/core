da1262bbefe7c8f0b7e8ff453a2b993c0ebc08e2
Matt Crowe <github.mpc@zym.me>
2017-12-14 22:34:50 +0000

    better clippable sort

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -21,7 +21,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.46';
+    const VERSION = '1.4.47';
 
     /**
      * The policy mappings for the application.
