8cd30b207f7d58b397edf7af40d1a9d15a6eb6d5
rob.lasota <rob.lasota@gmail.com>
2018-01-19 17:00:32 -0500

    updating the wysiwyg design

--- resources/belt/core/js/belt-bootstrap.js
+++ resources/belt/core/js/belt-bootstrap.js
@@ -1,7 +1,7 @@
 import Vuex from 'vuex';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
-import VueQuillEditor from 'vue-quill-editor'
+// import VueQuillEditor from 'vue-quill-editor'
 import 'admin-lte';
 import 'belt/core/js/adminlte/helper';
 import 'bootstrap-sass';
@@ -14,42 +14,42 @@ axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest'
 };
 
-VueQuillEditor.quillEditor.data = function() {
-    return {
-        _content: '',
-        defaultModules: {
-            toolbar: {
-                container: [
-                    ['bold', 'italic', 'underline', 'strike'],
-                    ['blockquote', 'code-block'],
-                    [{'header': 1}, {'header': 2}],
-                    [{'list': 'ordered'}, {'list': 'bullet'}],
-                    [{'script': 'sub'}, {'script': 'super'}],
-                    [{'indent': '-1'}, {'indent': '+1'}],
-                    [{'direction': 'rtl'}],
-                    [{'size': ['small', false, 'large', 'huge']}],
-                    [{'header': [1, 2, 3, 4, 5, 6, false]}],
-                    [{'color': []}, {'background': []}],
-                    [{'align': []}],
-                    ['clean'],
-                    ['link', 'image', 'video']
-                ],
-                handlers: {
-                    'image': function(value) {
-                        if (value) {
-                            var href = prompt('Enter an image URL');
-                            this.quill.format('image', href);
-                        } else {
-                            this.quill.format('image', false);
-                        }
-                    }
-                }
-            },
-        }
-    }
-};
+// VueQuillEditor.quillEditor.data = function() {
+//     return {
+//         _content: '',
+//         defaultModules: {
+//             toolbar: {
+//                 container: [
+//                     ['bold', 'italic', 'underline', 'strike'],
+//                     ['blockquote', 'code-block'],
+//                     [{'header': 1}, {'header': 2}],
+//                     [{'list': 'ordered'}, {'list': 'bullet'}],
+//                     [{'script': 'sub'}, {'script': 'super'}],
+//                     [{'indent': '-1'}, {'indent': '+1'}],
+//                     [{'direction': 'rtl'}],
+//                     [{'size': ['small', false, 'large', 'huge']}],
+//                     [{'header': [1, 2, 3, 4, 5, 6, false]}],
+//                     [{'color': []}, {'background': []}],
+//                     [{'align': []}],
+//                     ['clean'],
+//                     ['link', 'image', 'video']
+//                 ],
+//                 handlers: {
+//                     'image': function(value) {
+//                         if (value) {
+//                             var href = prompt('Enter an image URL');
+//                             this.quill.format('image', href);
+//                         } else {
+//                             this.quill.format('image', false);
+//                         }
+//                     }
+//                 }
+//             },
+//         }
+//     }
+// };
 
 Vue.use(Vuex);
-Vue.use(VueQuillEditor);
+// Vue.use(VueQuillEditor);
 Vue.use(VueResource);
 Vue.use(VueRouter);
---
+++ resources/belt/core/js/editors/tinymce.vue
@@ -0,0 +1,14 @@
+<template>
+    <vue-mce v-model="content"
+             v-on:input="updateValue"></vue-mce>
+</template>
+
+<script>
+    import { component } from 'vue-mce';
+    import editorMixins from 'belt/core/js/editors/mixins';
+
+    export default {
+        components: { 'vue-mce': component },
+        mixins: [editorMixins]
+    }
+</script>
