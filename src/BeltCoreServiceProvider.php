73d59a22a55657402900529c66e32d18ee941cb6
Matt Crowe <github.mpc@zym.me>
2018-03-27 20:59:49 +0000

    fix clip move

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -21,7 +21,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.78';
+    const VERSION = '1.4.79';
 
     /**
      * The policy mappings for the application.
