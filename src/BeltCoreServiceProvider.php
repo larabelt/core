5c02d7ea2651e3fefbe67fe6e32df353a0e1adb0
Matt Crowe <github.mpc@zym.me>
2018-03-29 15:15:15 +0000

    reduce CompileService cache duration

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -21,7 +21,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.4.79';
+    const VERSION = '1.4.80';
 
     /**
      * The policy mappings for the application.
