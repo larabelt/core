236a726d9b12ec07de9605600356b22723b2cb6d
Matt Crowe <github.mpc@zym.me>
2018-05-25 14:31:23 +0000

    class for team-input

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.5.2';
+    const VERSION = '1.5.4';
 
     /**
      * The policy mappings for the application.
