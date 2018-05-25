833ce242285357be4d8f8cd8cbc85eada29e9190
Matt Crowe <github.mpc@zym.me>
2018-05-25 19:16:46 +0000

    users.has_agreed for gdpr

--- src/Http/Requests/StoreUser.php
+++ src/Http/Requests/StoreUser.php
@@ -2,7 +2,7 @@
 
 namespace Belt\Core\Http\Requests;
 
-use Belt;
+use Belt, Auth;
 
 /**
  * Class StoreUser
@@ -16,14 +16,19 @@ class StoreUser extends Belt\Core\Http\Requests\UserRequest
      */
     public function rules()
     {
-
-        return [
+        $rules = [
             'email' => 'email|required|unique:users,email',
             'email_unique' => 'unique:users,email',
             'first_name' => 'required',
             'last_name' => 'required',
             'password' => 'required|confirmed|min:8',
         ];
+
+        if (!Auth::user()) {
+            $rules['has_agreed'] = 'required';
+        }
+
+        return $rules;
     }
 
 }
