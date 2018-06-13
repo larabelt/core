491450b5a86c005a2202d66babf5d49ac125dd77
Matt Crowe <github.mpc@zym.me>
2018-06-13 18:10:56 +0000

    admin user tabs tweaks

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -22,7 +22,7 @@ class BeltCoreServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.5.7';
+    const VERSION = '1.5.8';
 
     /**
      * The policy mappings for the application.
