64affebab0f7b1590da1d06e9ddba3a6686c2a14
Matt Crowe <github.mpc@zym.me>
2018-06-20 15:44:16 +0000

    auth permission for viewing user list

--- resources/belt/core/js/users/ctlr/shared.js
+++ resources/belt/core/js/users/ctlr/shared.js
@@ -4,10 +4,14 @@ export default {
     mixins: [access],
     created() {
         this.authCan('attach', 'roles');
+        this.authCan('view', 'users');
     },
     computed: {
         authCanAttachRoles() {
             return _.get(this.authAccess, 'roles.attach');
-        }
+        },
+        authCanViewUsers() {
+            return _.get(this.authAccess, 'view.users');
+        },
     },
 }
