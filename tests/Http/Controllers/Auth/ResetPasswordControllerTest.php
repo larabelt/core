bab8993383c506fb3ebbecc41c64f122e5889fbc
Matt Crowe <github.mpc@zym.me>
2018-06-25 17:57:23 +0000

    tests

--- tests/Http/Controllers/Auth/ResetPasswordControllerTest.php
+++ tests/Http/Controllers/Auth/ResetPasswordControllerTest.php
@@ -2,7 +2,7 @@
 
 use Belt\Core\Testing;
 use Belt\Core\Http\Controllers\Auth\ResetPasswordController;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 
@@ -12,6 +12,7 @@ class ResetPasswordControllerTest extends Testing\BeltTestCase
     /**
      * @cover \Belt\Core\Http\Controllers\Auth\ResetPasswordController::__construct
      * @cover \Belt\Core\Http\Controllers\Auth\ResetPasswordController::showResetForm
+     * @cover \Belt\Core\Http\Controllers\Auth\ResetPasswordController::redirectTo
      */
     public function test()
     {
@@ -22,6 +23,15 @@ class ResetPasswordControllerTest extends Testing\BeltTestCase
         $request = new Request();
         $view = $controller->showResetForm($request);
         $this->assertInstanceOf(View::class, $view);
+
+        # redirectTo (non-admin)
+        $this->assertEquals('/home', $controller->redirectTo());
+
+        # redirectTO (admin)
+        $user = factory(\Belt\Core\User::class)->make(['is_super' => true]);
+        Auth::shouldReceive('user')->andReturn($user);
+        $this->assertEquals('/admin', $controller->redirectTo());
+
     }
 
 }
