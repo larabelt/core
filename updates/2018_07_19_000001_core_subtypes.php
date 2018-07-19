341971ce01f7272ccc2a4954a1829b3ab5984e50
Matt Crowe <github.mpc@zym.me>
2018-07-19 20:54:44 +0000

    continue refactor template to subtype

--- updates/2018_07_19_000001_core_subtypes.php
+++ updates/2018_07_19_000001_core_subtypes.php
@@ -13,30 +13,17 @@ class BeltUpdateCoreSubtypes extends BaseUpdate
     public function up()
     {
         $this->info(sprintf('subtypes'));
-
+        $this->move();
     }
 
     public function move()
     {
-        //$tmpPath = $this->option('new-path', 'templates-tmp');
-        $tmpPath = config_path('belt/templates');
+        $old_path = config_path('belt/templates');
+        $new_path = config_path('belt/subtypes');
 
-        dump($tmpPath);
-
-
-        return;
-        if ($tmpPath && file_exists($tmpPath)) {
-            $targetPath = $this->option('target-path', 'templates');
-            $targetPath = config_path('belt/' . $targetPath);
-            if ($targetPath) {
-                if (file_exists($targetPath)) {
-                    $archivedPath = "$targetPath-archived";
-                    rename($targetPath, $archivedPath);
-                    $this->info("moved existing path to: $archivedPath");
-                }
-                rename($tmpPath, $targetPath);
-                $this->info("moved new path to: $targetPath");
-            }
+        if (file_exists($old_path)) {
+            rename($old_path, $new_path);
+            $this->info("rename config/belt/templates to config/belt/subtypes");
         }
     }
 
