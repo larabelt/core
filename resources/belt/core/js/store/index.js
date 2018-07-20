33e1f864b693740ebcc57c2720d0b2c447134846
Matt Crowe <github.mpc@zym.me>
2018-07-20 21:08:49 +0000

    clean up vuejs

--- resources/belt/core/js/store/index.js
+++ resources/belt/core/js/store/index.js
@@ -1,12 +1,12 @@
 import configs from 'belt/core/js/configs/store/global';
 import abilities from 'belt/core/js/abilities/store';
-import access from 'belt/core/js/access/store';
+//import access from 'belt/core/js/access/store';
 
 export default new Vuex.Store({
     modules: {
         abilities: abilities,
         configs: configs,
-        access: access,
+        //access: access,
     },
     state: {
 
