9311da341ad828c6c967942528243f7b04e8f001
Matt Crowe <github.mpc@zym.me>
2018-11-06 21:18:57 +0000

    continue translations

---
+++ resources/belt/core/js/inputs/QuasiCheckbox.vue
@@ -0,0 +1,32 @@
+<template>
+    <span>
+        <input type="hidden" v-model="form[column]"/>
+        <button class="btn btn-xs" :class="buttonClass" @click.prevent="toggle">
+            <i class="fa" :class="iconClass"></i>
+        </button>
+    </span>
+</template>
+<script>
+    import shared from 'belt/core/js/inputs/shared';
+
+    export default {
+        mixins: [shared],
+        computed: {
+            isActive() {
+                return this.form[this.column];
+            },
+            buttonClass() {
+                return this.isActive ? 'btn-primary' : 'btn-default';
+            },
+            iconClass() {
+                return this.isActive ? 'fa-check-square-o' : 'fa-square-o';
+            }
+        },
+        methods: {
+            toggle() {
+                this.form[this.column] = !this.form[this.column];
+                this.$emit('toggle');
+            }
+        }
+    }
+</script>
