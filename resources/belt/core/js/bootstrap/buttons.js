8c50395c4b1147d5a82a138dd301d1a52458c774
Matt Crowe <github.mpc@zym.me>
2018-11-06 21:35:25 +0000

    continue translations

--- resources/belt/core/js/bootstrap/buttons.js
+++ resources/belt/core/js/bootstrap/buttons.js
@@ -1,9 +1,11 @@
 import BaseButtonGroup from 'belt/core/js/button/BaseButtonGroup';
+import ButtonInlineTrash from 'belt/core/js/button/ButtonInlineTrash';
 import ButtonPreview from 'belt/core/js/button/ButtonPreview';
 import ButtonSave from 'belt/core/js/button/ButtonSave';
 import ButtonTranslate from 'belt/core/js/button/ButtonTranslate';
 
 Vue.component('base-button-group', BaseButtonGroup);
+Vue.component('button-inline-trash', ButtonInlineTrash);
 Vue.component('button-preview', ButtonPreview);
 Vue.component('button-save', ButtonSave);
 Vue.component('button-translate', ButtonTranslate);
---
+++ resources/belt/core/js/button/ButtonInlineTrash.vue
@@ -0,0 +1,54 @@
+<template>
+    <span>
+        <template v-if="!alert">
+            <button
+                    class="btn btn-default btn-xs"
+                    @click.prevent="trash"
+            ><i class="fa fa-trash"></i>
+            </button>
+        </template>
+        <template v-else>
+            <div class="btn-group">
+                <button
+                        class="btn btn-xs btn-warning"
+                        @click.prevent="confirm"
+                >trash</button>
+                <button
+                        class="btn btn-xs btn-default"
+                        @click.prevent="cancel"
+                >cancel</button>
+            </div>
+        </template>
+    </span>
+</template>
+
+<script>
+    import shared from 'belt/core/js/button/shared';
+
+    export default {
+        mixins: [shared],
+        data() {
+            return {
+                alert: false,
+            }
+        },
+        computed: {
+            buttonClass() {
+                return this.translationsVisible ? 'btn-warning' : 'btn-default';
+            },
+        },
+        methods: {
+            cancel() {
+                this.alert = false;
+            },
+            confirm() {
+                this.$emit('trash');
+                this.alert = false;
+            },
+            trash() {
+                this.alert = true;
+            },
+
+        },
+    }
+</script>
