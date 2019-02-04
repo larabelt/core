5a485e3b08bc3712dde1001cc61621cd10547e67
Matt Crowe <github.mpc@zym.me>
2019-02-04 19:13:11 +0000

    rtrim '/' in handled responses

--- src/BeltCoreServiceProvider.php
+++ src/BeltCoreServiceProvider.php
@@ -20,7 +20,7 @@ class BeltCoreServiceProvider extends Belt\Core\BeltServiceProvider
      *
      * @var string
      */
-    const VERSION = '2.0.4';
+    const VERSION = '2.0.5';
 
     /**
      * The policy mappings for the application.
