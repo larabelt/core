9e760da8f0f1dce4ae011317d797277f7b355ad3
Matt Crowe <github.mpc@zym.me>
2019-02-14 18:00:11 +0000

    continue unified documentation

--- resources/views/docs/partials/iframe.blade.php
+++ resources/views/docs/partials/iframe.blade.php
@@ -1,8 +1,9 @@
 @php
-
+    $height = isset($height) ? sprintf('height:%spx', $height) : '';
+    $style = "$height";
 @endphp
 
-<div class="embed-responsive embed-responsive-16by9">
+<div class="embed-responsive embed-responsive-16by9" style="{{ $style }}">
     <iframe
             class="embed-responsive-item"
             src="{{ $src }}"
