f515ea342ee598eae4848202c71e8048361718dc
rob.lasota <rob.lasota@gmail.com>
2020-03-18 15:27:11 -0400

    ability to add config to tinymce

--- resources/belt/core/js/editors/tinymce.vue
+++ resources/belt/core/js/editors/tinymce.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+    import _ from 'lodash';
     import TinyMCEVue from '@tinymce/tinymce-vue';
     import editorMixins from 'belt/core/js/editors/mixins';
 
@@ -15,12 +16,17 @@
         mixins: [editorMixins],
         data() {
             return {
-                init: {
+                init: {},
+                defaultInit: {
                     height: '400px',
-                    plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code wordcount',
+                    plugins: 'advlist autolink lists link image media charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime table paste code wordcount',
                     toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code'
                 }
             }
+        },
+        created() {
+            let override = _.get(window, 'larabelt.overrides.tinymce.init')
+            this.init = _.merge(this.defaultInit, override)
         },
         methods: {
             handleChange() {
